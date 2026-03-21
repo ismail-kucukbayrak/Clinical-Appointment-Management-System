1b7943c3a66a98ddf37aba65c2dce69773748520
İsmail Küçükbayrak <ismailkucukbayrak10@gmail.com>
2026-03-21 04:19:59 +0300

    Delete src directory

--- src/model/Patient.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Patient extends Person {
-    private static final long serialVersionUID = 1L;
-
-    public Patient(String name, long national_id) {
-        super(name, national_id);
-    }
-}
